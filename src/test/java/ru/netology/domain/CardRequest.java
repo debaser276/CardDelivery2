aa621218978495a21fed9b8f063eea1aca9e60d1
debaser <debaser86@yandex.ru>
2021-03-21 09:30:50 +0300

    add lombok plugin

--- src/test/java/ru/netology/domain/CardRequest.java
+++ src/test/java/ru/netology/domain/CardRequest.java
@@ -1,25 +1,12 @@
 package ru.netology.domain;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
 public class CardRequest {
     private final String city;
     private final String name;
     private final String phone;
-
-    public CardRequest(String city, String name, String phone) {
-        this.city = city;
-        this.name = name;
-        this.phone = phone;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
 }
